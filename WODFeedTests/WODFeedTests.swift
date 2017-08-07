cf94cafc76f31148552fce668165c1f4e711f5bc
Merrick Sapsford <merrick@sapsford.tech>
2017-08-07 15:26:23 +0100

    Add project

---
+++ WODFeedTests/WODFeedTests.swift
@@ -0,0 +1,36 @@
+//
+//  WODFeedTests.swift
+//  WODFeedTests
+//
+//  Created by Merrick Sapsford on 07/08/2017.
+//  Copyright © 2017 AMRAP Labs. All rights reserved.
+//
+
+import XCTest
+@testable import WODFeed
+
+class WODFeedTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
