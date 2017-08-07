2268eae0496cf6950dcc317026160b145dc603ac
Merrick Sapsford <merrick@sapsford.tech>
2017-08-07 15:41:41 +0100

    Add basic feed

---
+++ WODReader/Models/Feed.swift
@@ -0,0 +1,22 @@
+//
+//  Feed.swift
+//  WODReader
+//
+//  Created by Merrick Sapsford on 07/08/2017.
+//  Copyright © 2017 AMRAP Labs. All rights reserved.
+//
+
+import Foundation
+
+class Feed {
+    
+     // MARK: Properties
+    
+    let url: URL
+    
+    // MARK: Init
+    
+    init(url: URL) {
+        self.url = url
+    }
+}
---
+++ WODReader/WODReader.swift
@@ -0,0 +1,18 @@
+//
+//  WODReader.swift
+//  WODReader
+//
+//  Created by Merrick Sapsford on 07/08/2017.
+//  Copyright © 2017 AMRAP Labs. All rights reserved.
+//
+
+import Foundation
+
+class WODReader {
+    
+    // MARK: Init
+    
+    init(feeds: [Feed]) {
+        
+    }
+}
